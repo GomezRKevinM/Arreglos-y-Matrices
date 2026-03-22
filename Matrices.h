9d1b2b4777a5d84aaa83e49a7073c219a7976d3d
kevin-aspire <kgomezr2@unicartagena.edu.co>
2026-03-22 07:46:35 -0500

    feat: Matrices.h created and complete - Kevin Gómez

--- Matrices.h
+++ Matrices.h
@@ -1,10 +1,17 @@
 #ifndef Matriz
 #define Matriz
 
-namespace Matrices {
-    class Matrices {
+#include <vector>
 
-    };
+
+namespace Matrices {
+    void imprimirTable(std::vector<std::vector<int>> matriz);
+
+    void recorrerPorColumnas(std::vector<std::vector<int>> matriz);
+
+    int sumarMatriz(std::vector<std::vector<int>> matriz);
+
+    void intercambiarFilas(std::vector<std::vector<int>>& matriz,int fila1, int fila2);
 }
 
 #endif
