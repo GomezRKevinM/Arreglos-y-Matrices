e97ee52d7936e68f76b8e5c637f365bb67e86fe3
gcastanom <gcastano@unicartagena.edu.co>
2026-03-23 19:10:38 -0500

    Recorrido e impresión

--- Gary/Ts/index.ts
+++ Gary/Ts/index.ts
@@ -5,3 +5,13 @@ let numeros: number[];
 numeros = Utils.getRandomListInt(10, 1, 100);
 
 console.log(numeros);
+
+console.log("recorriendo por bucle For");
+for (let i = 0; i < numeros.length; i++) {
+    console.log(`\tElemento ${i}: ${numeros[i]}`);
+}
+
+console.log("recorriendo por bucle ForEach");
+numeros.forEach((element, index) => {
+    console.log(`\tElemento ${index}: ${element}`);
+});
