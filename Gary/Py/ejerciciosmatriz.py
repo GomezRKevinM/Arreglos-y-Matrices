0bda4b781f4bec23d7b6f55d9ab62f8563f21b59
gcastanom <gcastano@unicartagena.edu.co>
2026-03-27 10:19:45 -0500

    Ejercicios Matrices

---
+++ Gary/Py/ejerciciosmatriz.py
@@ -0,0 +1,41 @@
+matriz = []
+contador = 1
+
+for i in range(3):
+    fila = []
+    for j in range(3):
+        fila.append(contador)
+        contador += 1
+    matriz.append(fila)
+
+print("Matriz:")
+for fila in matriz:
+    print(fila)
+
+print("Imprimir la matriz en forma de tabla:")
+for i in range(len(matriz)):
+    for j in range(len(matriz[i])):
+        print(matriz[i][j], end="\t")
+    print()
+
+
+print("Recorrer por columnas:")
+for j in range(len(matriz[0])):
+    for i in range(len(matriz)):
+        print(matriz[i][j], end="\t")
+    print()
+
+print("Sumar todos los elementos:")
+suma = 0
+
+for i in range(3):
+    for j in range(3):
+        suma += matriz[i][j]
+
+print("Suma total:", suma)
+
+matriz[0], matriz[2] = matriz[2], matriz[0]
+
+print("Intercambiar la primera fila con la ultima:")
+for fila in matriz:
+    print(fila)
