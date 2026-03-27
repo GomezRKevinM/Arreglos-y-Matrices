6bac61abaf562eff14c9431b1f8fd2013a745ae1
gcastanom <gcastano@unicartagena.edu.co>
2026-03-27 12:50:10 -0500

    Orden y Estetica

--- Gary/Ts/index.ts
+++ Gary/Ts/index.ts
@@ -1,30 +1,34 @@
 import {Utils} from "./Utils";
 
-// ***ARREGLOS***-----------------------------------------------
+console.log("**********************************");
+console.log("         ***ARREGLOS***");
 
-
+console.log("");
+console.log("arreglo de 10 numeros enteros aleatorios: ");
 let numeros: number[];
-
 numeros = Utils.getRandomListInt(10, 1, 100);
-
 console.log(numeros);
+console.log("");
+
 
 console.log("recorriendo por bucle For");
 for (let i = 0; i < numeros.length; i++) {
     console.log(`\tElemento ${i}: ${numeros[i]}`);
 }
+console.log("");
 
 console.log("recorriendo por bucle ForEach");
 numeros.forEach((valor, index) => {
     console.log(`\tElemento ${index}: ${valor}`);
 });
-
+console.log("");
+console.log("Cambiar todos los valores impares por cero");
 Utils.modificarImpares(numeros);
-
 console.log(numeros);
-
 let numeros2: number[] = Utils.getRandomListInt(10, 1, 100);
 
+console.log("");
+console.log("Multiplicar todos los valores por su índice");
 console.log("Nuevo arreglo:"+ numeros2);
 
 numeros2.forEach((valor,index) => {
@@ -33,14 +37,18 @@ numeros2.forEach((valor,index) => {
     console.log(`\tElemento ${index}: ${valor} * ${index} = ${numeros2[index]}`);
 
 })
-
 console.log("Nuevo arreglo:"+ numeros2);
 
+console.log("");
+console.log("Buscar un elemento en el arreglo");
+
 let posicion: number = Utils.encontrarElemento(numeros2, 0);
-
 console.log((posicion >= 0) ? `El elemento se encuentra en la posición ${posicion}` : "El elemento no se encuentra en el arreglo");
+console.log("");
 
-// ***MATRICES***----------------------------------------------
+console.log("*************************************");
+console.log("         ***MATRICES***");
+console.log("");
 
 let matriz: number[][] = [];
 let contador = 1;
@@ -53,7 +61,8 @@ for (let i = 0; i < 3; i++) {
 console.log("Matriz inicial:");
 console.table(matriz);
 
-
+console.log("");
+console.log("Imprimir la matriz en forma de tabla:");
 for (let i = 0; i < matriz.length; i++) {
     let fila = "";
     for (let j = 0; j < matriz[i].length; j++) {
@@ -62,7 +71,8 @@ for (let i = 0; i < matriz.length; i++) {
     console.log(fila);
 }
 
-
+console.log("");
+console.log("Recorrerla por columnas:");
 for (let j = 0; j < 3; j++) {
     let columna = "";
     for (let i = 0; i < 3; i++) {
@@ -71,7 +81,8 @@ for (let j = 0; j < 3; j++) {
     console.log(columna);
 }
 
-
+console.log("");
+console.log("Sumar todos los elementos:");
 let suma = 0;
 for (let i = 0; i < 3; i++) {
     for (let j = 0; j < 3; j++) {
@@ -80,9 +91,10 @@ for (let i = 0; i < 3; i++) {
 }
 console.log("Suma total de la matriz:", suma);
 
-
+console.log("");
+console.log("Intercambiar la primera, por la ultima fila:");
 let temp = matriz[0];
 matriz[0] = matriz[2];
 matriz[2] = temp;
-console.log("Matriz después del intercambio:");
+console.log("Matriz despues del intercambio:");
 console.table(matriz);
